d6e1907cf6d98bd3c114ec6f53fb0152537a3c31
yfn <yfn@infeini.com>
2018-05-09 18:02:41 +0800

    fix error

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,25 @@
 //  Copyright © 2018年 infeini. All rights reserved.
 //
 #include "main.h"
-void server(int port){
+
+void server(int port) {
     int socketId;
-    if((socketId=socket(AF_INET, SOCK_STREAM, 0))==-1)
+    if ((socketId = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         throw "socket创建失败";
     else
-        log("socketId:"+std::to_string(socketId));
+        log("socketId:" + std::to_string(socketId));
 
     struct sockaddr_in myAddress;
-    myAddress.sin_family=AF_INET;
-    myAddress.sin_port=htons(port);
+    myAddress.sin_family = AF_INET;
+    myAddress.sin_port = htons(port);
     myAddress.sin_addr.s_addr = INADDR_ANY;
-    bzero(&(myAddress.sin_zero),8);
-    if(bind(socketId, (struct sockaddr *)&myAddress, sizeof(struct sockaddr))==-1)
+    bzero(&(myAddress.sin_zero), 8);
+    if (bind(socketId, (struct sockaddr *) &myAddress, sizeof(struct sockaddr)) == -1)
         throw "server bind出错!";
     else
         log("server bind成功");
-    
-    if(listen(socketId, 8) == -1)
+
+    if (listen(socketId, 8) == -1)
         throw "server listen出错";
     else
         log("server listen成功");
@@ -33,65 +34,66 @@ void server(int port){
     int bufferSize = 2048;
     char buffer[bufferSize];
     while (true) {
-        try{
-        int clientId;
-        if((clientId=accept(socketId, (struct sockaddr *)&remoteAddress, (socklen_t *)&inLength))==-1)
-            log("server 接受成功");
-        else
-            log("server 接收失败");
-        int length;
-        if((length=read(clientId, buffer, bufferSize))!=-1){
-            buffer[length] = '\0';
-            
-            std::string logStr = "server 收到:(";
-            logStr.append(std::to_string(length));
-            logStr.append(")");
-            logStr.append(buffer);
-            log(logStr);
-        }else throw "server 读取失败";
-        std::string sendData = "hi";
-        if(send(clientId,sendData.c_str(),sendData.length(), 0)==-1)
-            throw "server 发送失败";
-        else
-            log("server 发送成功");}
-        catch(const char* error)
-        {
+        try {
+            int clientId;
+            if ((clientId = accept(socketId, (struct sockaddr *) &remoteAddress, (socklen_t *) &inLength)) == -1)
+                log("server 接受成功");
+            else
+                log("server 接收失败");
+            int length;
+            if ((length = read(clientId, buffer, bufferSize)) != -1) {
+                buffer[length] = '\0';
+
+                std::string logStr = "server 收到:(";
+                logStr.append(std::to_string(length));
+                logStr.append(")");
+                logStr.append(buffer);
+                log(logStr);
+            } else throw "server 读取失败";
+            std::string sendData = "hi";
+            if (send(clientId, sendData.c_str(), sendData.length(), 0) == -1)
+                throw "server 发送失败";
+            else
+                log("server 发送成功");
+        }
+        catch (const char *error) {
             std::string errorStr = "发生异常:";
             errorStr.append(error);
             log(errorStr);
         }
     }
 }
-void client(const char* domain,int port){
+
+void client(const char *domain, int port) {
     int socketId = socket(AF_INET, SOCK_STREAM, 0);
-    struct hostent* host = gethostbyname(domain);
-    if(socketId==-1)
+    struct hostent *host = gethostbyname(domain);
+    if (socketId == -1)
         throw "client 创建socket失败";
     else
-        log("client socketId:"+std::to_string(socketId));
+        log("client socketId:" + std::to_string(socketId));
     struct sockaddr_in address;
-    address.sin_family=AF_INET;
-    address.sin_port=htons(port);
-    address.sin_addr = *((struct in_addr *)host->h_addr);
-    int connectId = connect(socketId, (struct sockaddr*)&address, sizeof(struct sockaddr));
-    if(connectId==-1)
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr = *((struct in_addr *) host->h_addr);
+    int connectId = connect(socketId, (struct sockaddr *) &address, sizeof(struct sockaddr));
+    if (connectId == -1)
         throw "client 连接失败";
     else
-        log("client connectId:"+std::to_string(connectId));
-    
-    //模拟http请求
-    std::string head = getHttpHead(domain);
+        log("client connectId:" + std::to_string(connectId));
 
-    if(send(socketId, head.c_str(), head.length(), 0)==-1)
+    //模拟http请求
+    std::string head = getHttpRequestData(domain, port,"/");
+
+    if (send(socketId, head.c_str(), head.length(), 0) == -1)
         throw "client 发送失败";
     else
         log("client 发送成功");
     long length;
     int bufferSize = 2048;
     char buffer[bufferSize];
-    if((length = recv(socketId, buffer, bufferSize, 0))==-1)
+    if ((length = recv(socketId, buffer, bufferSize, 0)) == -1)
         throw "client 接受数据错误";
-    else{
+    else {
         buffer[length] = '\0';
         std::string logStr = "client 收到(";
         logStr.append(std::to_string(length));
@@ -100,10 +102,12 @@ void client(const char* domain,int port){
         log(logStr.c_str());
     }
 }
-int main(int argc, const char * argv[]) {
+
+int main(int argc, const char *argv[]) {
     // insert code here...
-    std::thread serverThread = std::thread(server,8080);
-    sleep(2);
-    client("localhost", 8080);
+//    std::thread serverThread = std::thread(server, 8080);
+//    sleep(2);
+    client("www.infeini.com", 80);
+//    serverThread.join();
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -18,22 +18,23 @@
 #include <unistd.h>
 #include <time.h>
 #include <iostream>
+
 #ifndef main_h
 #define main_h
 
-void log(std::string log)
-{
-    std::cout<<log<<std::endl;
+void log(std::string log) {
+    std::cout << log << std::endl;
 }
-std::string getHttpHead(const char* domain)
-{
-    std::string head = "GET / HTTP/1.1\r\n";
-    head.append("Host: ");
-    head.append(domain);
-    head.append("\r\n");
-    head.append("baidu is sb");
-    head.append("\r\n");
-    head = "GET / HTTP/1.1\r\nHost: www.baidu.com:80\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: zh-CN,zh;q=0.9";
+
+std::string getHttpRequestData(const char *domain, int port, const char *path) {
+    std::string head = "GET http://";
+    head.append(domain)
+            .append(path)
+            .append("  HTTP/1.1\\r\\nHost: ")
+            .append(domain)
+            .append(":")
+            .append(std::to_string(port)).append("\\r\\n\\r\\n");
     return head;
 }
+
 #endif /* main_h */
